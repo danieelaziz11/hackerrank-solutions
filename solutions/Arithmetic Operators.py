98e4d4b635e950ad1d19011b929c2dfba9915c20
Daniel Aziz <danieelaziz11@gmail.com>
2026-07-21 16:15:55 +0300

    Add new HackerRank solutions

---
+++ solutions/Arithmetic Operators.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input("Enter first number :"))
+    b = int(input("Enter second number :"))
+    print (a+b)
+    print (a-b)
+    print (a*b)
---
+++ solutions/Division.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    a = int(input("Enter first number :"))
+    b = int(input("Enter second number :"))
+    print (int(a/b))
+    print (float(a/b))
